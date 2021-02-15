89ec0552f89cde8005973412ff9672858e2fed5c
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-15 15:56:21 +0100

    Attempt to move the description to the separate component

--- app/script.js
+++ app/script.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import PropTypes from 'prop-types';
+import AppDescription from './AppDescription';
 
 class App extends React.Component {
   state = {
@@ -16,10 +16,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Protect your eyes</h1>
-        {(status === 'off') && <div>
-          <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-          <p>This app will help you track your time and inform you when it's time to rest.</p>
-        </div>}
+        {(status === 'off') && <AppDescription />}
         {(status === 'work') && <img src="./images/work.png" />}
         {(status === 'rest') && <img src="./images/rest.png" />}
         {(status !== 'off') && <div className="timer">18:23</div>}
